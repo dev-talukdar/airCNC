09bf455fefeaf0a064e92aeb86daca96941bad6f
dev.talukdar <khejurbd@gmail.com>
2023-05-31 08:49:36 +0600

    added

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+import NavigationBar from "../../Shared/NavigationBar/NavigationBar";
+import Footer from "../../Shared/Footer/Footer";
+
+ 
+
+const Home = () => {
+    return (
+        <div>
+            
+            <h2>it's Home page</h2>
+        </div>
+    );
+};
+
+export default Home;
